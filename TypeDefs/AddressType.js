e5674fa10841358751899b70c1f46c90d559b31c
Nick Adams <nick.pappas.adams@gmail.com>
2022-11-08 18:56:57 -0600

    format refactor

--- TypeDefs/AddressType.js
+++ TypeDefs/AddressType.js
@@ -1,17 +1,14 @@
-const graphql = require('graphql')
-const {
-    GraphQLObjectType,
-    GraphQLString,
-  } = graphql;
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString } = graphql;
 
 const AddressType = new GraphQLObjectType({
-    name: "Address",
-    fields: () => ({
-      suite: { type: GraphQLString },
-      street: { type: GraphQLString },
-      city: { type: GraphQLString },
-      zipcode: { type: GraphQLString },
-    }),
-  });
+  name: "Address",
+  fields: () => ({
+    suite: { type: GraphQLString },
+    street: { type: GraphQLString },
+    city: { type: GraphQLString },
+    zipcode: { type: GraphQLString },
+  }),
+});
 
-  module.exports = AddressType;
+module.exports = AddressType;
--- TypeDefs/PeopleType.js
+++ TypeDefs/PeopleType.js
@@ -1,26 +1,20 @@
-const graphql = require('graphql')
-const {
-    GraphQLObjectType,
-    GraphQLInt,
-    GraphQLString,
-  } = graphql;
-  const AddressType = require("./AddressType")
-  const CompanyType = require('./CompanyType')
-
-
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLInt, GraphQLString } = graphql;
+const AddressType = require("./AddressType");
+const CompanyType = require("./CompanyType");
 
 const PeopleType = new GraphQLObjectType({
-    name: "Person",
-    fields: () => ({
-      id: { type: GraphQLInt },
-      name: { type: GraphQLString },
-      username: { type: GraphQLString },
-      email: { type: GraphQLString },
-      phone: { type: GraphQLString },
-      website: { type: GraphQLString },
-      address: { type: AddressType },
-      company: { type: CompanyType }
-    }),
-  });
+  name: "Person",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    username: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+    website: { type: GraphQLString },
+    address: { type: AddressType },
+    company: { type: CompanyType },
+  }),
+});
 
-  module.exports = PeopleType;
+module.exports = PeopleType;
