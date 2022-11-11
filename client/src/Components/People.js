30301bf02e5431ac7437fcb43162dc40dbde669a
Nick Adams <nick.pappas.adams@gmail.com>
2022-11-11 13:28:57 -0600

    destructured adddress prop in people

--- client/src/Components/People.js
+++ client/src/Components/People.js
@@ -15,18 +15,18 @@ const PEOPLE_QUERY = gql`
     }
   }
 `;
-function People () {
+
+const People = () => {
     const { loading, error, data } = useQuery(PEOPLE_QUERY)
     if(loading) return <p>Loading...</p>
     if(error) return <p>Error</p>
-    console.log(data)
 
-    return data.people.map(({ id, name, username, phone, address }) => (
+    return data.people.map(({ id, name, username, phone, address: {street, suite, city} }) => (
         <div key={id}>
             <h1>Name: {name}</h1>
             <h3>Username: {username}</h3>
             <h3>Phone: {phone}</h3>
-            <h3>Address: {address.street}, {address.city}</h3>
+            <h3>Address: {street}, {suite}, {city}</h3>
         </div>
     ))
   
